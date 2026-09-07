51b04267f9e4ec28bdbac275255555578536f3be
rwatts3 <ryandwatts@gmail.com>
2026-09-07 00:39:38 -0700

    Add type declarations for iOS 27 token CSS imports.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
 
+declare module '@ios27_design_system/tokens/css'
+declare module '@ios27_design_system/tokens/css/materials'
+declare module '@ios27_design_system/tokens/css/typography'
+
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_PUBLISHABLE_KEY: string
